77f774a42437a465c1019f0fa5f3f83182dd57d7
Екатерина Ложенко <lozhkat@gmail.com>
2023-09-26 12:25:40 +0300

    -

---
+++ lettercombination.py
@@ -0,0 +1,23 @@
+def letterCombinations(digits) :
+    l={'2':'abc',
+       '3':'def',
+       '4':'ghi',
+       '5':'jkl',
+       '6':'mno',
+       '7':'pqrs',
+       '8':'tuv',
+       '9':'wxyz'}
+    res=[]
+    import string
+
+    def backtrack(i,curstr):
+        if len(curstr)==len(digits):
+            res.append(curstr)
+            return
+        for c in l[digits[i]]:
+            backtrack(i+1,curstr+c)
+    if digits:
+        backtrack(0,'')
+    return res
+
+print(letterCombinations(digits = "23"))
